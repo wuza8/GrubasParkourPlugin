8f01100c3f47843d61905d840b22a7ab9fb00b98
ProKoksPL1 <tmalecki74@gmail.com>
2023-10-15 18:31:47 +0200

    commit dodanie

--- src/main/java/aybici/parkourplugin/Announcmenter.java
+++ src/main/java/aybici/parkourplugin/Announcmenter.java
@@ -8,15 +8,15 @@ import java.util.List;
 
 public class Announcmenter {
 
-    private static long mins = 1; //Wysyłaj wiadomość co minutę
+    private static long mins = 11; //Wysyłaj wiadomość co 11 minut
 
     private static List<String> messages = new ArrayList<>();
 
     private static int current = 0;
 
     public static void run(){
-        messages.add(ChatColor.RED+ "Kup wipa");
-        messages.add(ChatColor.RED+ "Wejdź na disco");
+        messages.add(ChatColor.AQUA+ ">"+ ChatColor.GREEN + "> "+ ChatColor.WHITE + "Zakup usługę "+ ChatColor.YELLOW + "VipMan "+ ChatColor.WHITE + "na naszym sklepie już teraz! Wszystkie dodatkowe przywileje możesz sprawdzić na naszej stronie: "+ ChatColor.YELLOW + "http://itemshop.pl/842866.html");
+        messages.add(ChatColor.AQUA+ ">"+ ChatColor.GREEN + "> "+ ChatColor.WHITE + "Wejdź na naszego "+ ChatColor.BLUE + "discorda"+ ChatColor.WHITE + ", aby być na bieżąco - "+ ChatColor.BLUE + "https://discord.gg/AAAkcX3zae");
 
         Bukkit.getScheduler().runTaskTimer(ParkourPlugin.getInstance(), ()->{
             Bukkit.broadcastMessage(messages.get(current));
